9f470d7f36d137f47abb01d2982dc6f4c6947fc3
villemsusi <villem.susi@gmail.com>
2019-06-26 08:51:59 +0300

    Uploaded default files

--- index.php
+++ index.php
@@ -2,9 +2,30 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Praktika</title>
+    <title>Rets</title>
+    <link rel="stylesheet" href="artsy.css">
   </head>
   <body>
-    Testest
+    <div id="wrapper">
+        <div id="menu" class="menu menu-block" style="display:none">
+            <button onclick="menu_close()">&times;</button><br>
+            <a href="#">Home</a><br>
+            <a href="#">Tasks</a><br>
+            <a href="#">Gallery</a><br>
+        </div>
+        <div id="content">
+            <button onclick="menu_open()">☰</button>
+        </div>
+    </div>
+
+    <script>
+    function menu_open() {
+        document.getElementById("menu").style.display = "block";
+    }
+
+    function menu_close() {
+        document.getElementById("menu").style.display = "none";
+    }
+    </script>
   </body>
 </html>
